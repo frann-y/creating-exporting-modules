807a742a577381823086c8ff2e2924be9702513b
frann-y <fgranger00@gmail.com>
2019-01-11 21:52:59 +0000

    exporting file

---
+++ listModule.js
@@ -0,0 +1,24 @@
+var output = []; //made empty array
+
+function numberPush(number) { //pushing numbers to the array
+  output.push(number)
+};
+
+function printList() { //prints the output of my list
+  console.log(output);
+}
+
+function sortOutput () { //sorts my list
+  output.sort(function(a, b){return a-b});
+  return output;
+}
+
+function getSorted () {
+  console.log(sortOutput());
+}
+
+module.exports = {
+  numberPush: numberPush,
+  printList: printList,
+  getSorted,
+}
