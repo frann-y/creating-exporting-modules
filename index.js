12f8b31a74208bba757da8273f60d9756d396b8c
frann-y <fgranger00@gmail.com>
2019-01-11 21:53:29 +0000

    importing file

---
+++ index.js
@@ -0,0 +1,10 @@
+//require the module
+var list = require("./listModule"); //naming my export page to a variable to call it easily
+
+list.numberPush(5);
+list.numberPush(7);
+list.numberPush(2);
+list.numberPush(1);
+list.numberPush(9);
+list.printList();
+list.getSorted();
